ea426ab86c7e139f7a281a6ea20aa9ffd9374866
dimasm93 <software.dimas_m@icloud.com>
2022-03-19 02:45:18 +0700

    using escape characters

--- query/01-select/01-select-all-data.sql
+++ query/01-select/01-select-all-data.sql
@@ -10,3 +10,11 @@ SELECT department_id kode_divisi,
        location_id "$ kode lokasi *",
        manager_id as " tes^&#@$%"
 FROM departments;
+
+SELECT E'*',
+       E'()asadfsadf',
+       E'asldfkjdslkfj\tasd\nfsdf',
+       E'[]';
+
+SELECT department_name ||  E'\n' || departments.department_id
+from departments;
