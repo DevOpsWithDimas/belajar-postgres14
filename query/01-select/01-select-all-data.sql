b2e704424b2b1512e2c6d9142d7fc566a8ad71df
dimasm93 <software.dimas_m@icloud.com>
2022-03-19 02:28:30 +0700

    column lable and alias

--- query/01-select/01-select-all-data.sql
+++ query/01-select/01-select-all-data.sql
@@ -4,3 +4,9 @@ FROM departments;
 SELECT department_id,
        department_name
 FROM departments;
+
+SELECT department_id kode_divisi,
+       department_name as n$2ama_divisi,
+       location_id "$ kode lokasi *",
+       manager_id as " tes^&#@$%"
+FROM departments;
