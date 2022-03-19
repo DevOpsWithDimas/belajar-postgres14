93c7f43358bd0cfc60fb4957b28699fcb139bdc4
dimasm93 <software.dimas_m@icloud.com>
2022-03-20 02:45:53 +0700

    sql date/time function

--- query/01-select/03-functions.sql
+++ query/01-select/03-functions.sql
@@ -4,8 +4,15 @@ SELECT concat(first_name, ' ', last_name) as      fullname,
        length(concat(first_name, ' ', last_name)) length_from_concat
 from employees;
 
-SELECT 3 * 3 as multiply,
-       div(10, 3) as division,
-       mod(10, 3) as modulus,
+SELECT 3 * 3      as    multiply,
+       div(10, 3) as    division,
+       mod(10, 3) as    modulus,
        round(5.4475, 2) rounding_up,
-       floor(6.9) floor;
+       floor(6.9)       floor;
+
+SELECT employee_id,
+       current_timestamp,
+       current_date,
+       start_date                         as mulai_kerja,
+       extract(year from age(current_timestamp, start_date)) as tahun_kerja
+FROM job_history;
