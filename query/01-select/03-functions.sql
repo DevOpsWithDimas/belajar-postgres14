acee0ac41fd2eb2c8e924b866516bc10444fdb00
dimasm93 <software.dimas_m@icloud.com>
2022-03-20 02:25:10 +0700

    sql math function

--- query/01-select/03-functions.sql
+++ query/01-select/03-functions.sql
@@ -3,3 +3,9 @@ SELECT concat(first_name, ' ', last_name) as      fullname,
        reverse(last_name)                 as      last_name_reverse,
        length(concat(first_name, ' ', last_name)) length_from_concat
 from employees;
+
+SELECT 3 * 3 as multiply,
+       div(10, 3) as division,
+       mod(10, 3) as modulus,
+       round(5.4475, 2) rounding_up,
+       floor(6.9) floor;
