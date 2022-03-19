11d37146b15bb4e7dfddba583dee9df759ca736b
dimasm93 <software.dimas_m@icloud.com>
2022-03-19 21:37:31 +0700

    sql math operator

---
+++ query/01-select/02-select-operators.sql
@@ -0,0 +1,17 @@
+SELECT 2 + 2  as tambah,
+       2 * 3  as kali,
+       10 % 2 as mod,
+       2 ^ 3  as pangkat;
+
+
+SELECT first_name,
+       salary,
+       commission_pct,
+       salary * commission_pct as bonus
+from employees;
+
+
+SELECT current_date - 2                      as lusa,
+       current_date + 1                      as besok,
+       current_timestamp                     as sekarang,
+       current_timestamp + interval '2 hour' as "kurang 2 jam";
