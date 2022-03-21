b6054e5ac75ab154e18ad21bcb93d9d7a1d9352c
dimasm93 <software.dimas_m@icloud.com>
2022-03-22 04:02:07 +0700

    sql where clause dengan like predicates

--- query/01-select/04-select-where-clause.sql
+++ query/01-select/04-select-where-clause.sql
@@ -5,3 +5,15 @@ WHERE employee_id = 101;
 SELECT employee_id, first_name, salary
 FROM employees
 WHERE salary <= 17000;
+
+SELECT *
+FROM employees
+WHERE first_name like 'S%';
+
+SELECT *
+FROM employees
+WHERE first_name like '__a%';
+
+SELECT *
+FROM employees
+WHERE first_name ilike 's%';
