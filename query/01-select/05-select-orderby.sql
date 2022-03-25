8377d254b2e5e96f4c20458f79460ea0f87fc92d
dimasm93 <software.dimas_m@icloud.com>
2022-03-26 01:27:21 +0700

    deep dive into sort expression

--- query/01-select/05-select-orderby.sql
+++ query/01-select/05-select-orderby.sql
@@ -9,3 +9,20 @@ ORDER BY department_id DESC;
 SELECT *
 FROM departments
 ORDER BY department_name DESC;
+
+SELECT employee_id,
+       first_name,
+       salary,
+       COALESCE(commission_pct, 0)             commission_digit,
+       salary * coalesce(commission_pct, 0) as commission_currency
+FROM employees
+order by salary * COALESCE(commission_pct, 0);
+
+
+SELECT employee_id,
+       first_name,
+       salary,
+       COALESCE(commission_pct, 0)             commission_digit,
+       salary * coalesce(commission_pct, 0) as commission_currency
+FROM employees
+order by commission_digit desc;
