546d020ebf1a2f55f733686fb68197642462eed3
dimasm93 <software.dimas_m@icloud.com>
2022-04-29 01:33:05 +0700

    group by clause with aggregate function

--- query/01-select/09-aggregate-functions.sql
+++ query/01-select/09-aggregate-functions.sql
@@ -13,3 +13,21 @@ from employees;
 select avg(salary) as average_salary
 from employees
 where job_id = 'IT_PROG';
+
+
+--- group by query
+
+-- select distinct job_id
+-- from employees;
+
+SELECT job_id,
+       count(employee_id) as count_employee_by_job,
+       max(salary)        as max_salary_employee_by_job,
+       sum(salary)        as sum_salary_employee_by_job
+from employees
+group by job_id;
+
+select department_id, job_id, count(*) as employee_by_dep_and_job
+from employees
+group by department_id, job_id
+order by job_id;
