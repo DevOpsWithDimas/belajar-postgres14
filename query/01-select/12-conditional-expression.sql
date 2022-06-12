00b70bbbd07133dc99c79fdbc2d7b7de6cdff865
dimasm93 <software.dimas_m@icloud.com>
2022-06-13 02:50:28 +0700

    sql conditional expression with case-when-else

--- query/01-select/12-conditional-expression.sql
+++ query/01-select/12-conditional-expression.sql
@@ -8,3 +8,16 @@ select employee_id,
                then 'Tidak memiliki komisi!'
            end as keternangan
 from employees;
+
+-- using condition expression with case-when-else
+select employee_id    as kode_karyawan,
+       first_name     as nama_karyawan,
+       salary         as gaji_sebulan,
+       commission_pct as komisi,
+       case
+           when commission_pct is null then 'Tidak memiliki komisi!'
+           when commission_pct = 0.2 then 'Komisinya sama dengan 20%'
+           when commission_pct >= 0.2 then 'Komisinya lebih besar dari 20%'
+           else 'Komisi lebih kecil dari 20%'
+           end        as keterangan
+from employees;
