feadfaee288e2526108adf5565dd711eaf728509
dimasm93 <software.dimas_m@icloud.com>
2022-07-05 14:14:18 +0700

    sql combine query using intersect statement

--- query/01-select/13-combine-queries.sql
+++ query/01-select/13-combine-queries.sql
@@ -17,3 +17,28 @@ UNION ALL
 select *
 from (values (1, 'dimasm93', 'Dimas Maryanto', true),
              (4, 'abdul', 'Abdul Array', false)) as data2;
+
+-- combine query using intersect with remove duplicate rows
+select *
+from (values (1, 'dimasm93', 'Dimas Maryanto', true),
+             (2, 'myusuf', 'Muhamad Yusuf', false),
+             (3, 'mpurwadi', 'Muhamad Purwadi', true)) as data1
+INTERSECT DISTINCT
+select *
+from (values (1, 'dimasm93', 'Dimas Maryanto', true),
+             (4, 'abdul', 'Abdul array', false),
+             (3, 'mpurwadi', 'Muhamad Purwadi', true)) as data2
+
+-- combine query using intersect with duplicate rows
+select *
+from (values (1, 'dimasm93', 'Dimas Maryanto', true),
+             (2, 'myusuf', 'Muhamad Yusuf', false),
+             (2, 'myusuf', 'Muhamad Yusuf', false),
+             (3, 'mpurwadi', 'Muhamad Purwadi', true)) as data1
+INTERSECT ALL
+select *
+from (values (1, 'dimasm93', 'Dimas Maryanto', true),
+             (2, 'myusuf', 'Muhamad Yusuf', false),
+             (2, 'myusuf', 'Muhamad Yusuf', false),
+             (4, 'abdul', 'Abdul array', false),
+             (3, 'mpurwadi', 'Muhamad Purwadi', true)) as data2
