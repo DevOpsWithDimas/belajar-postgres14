47adc9a6eddb59e87c5dbc24a3c4d05d6e2f3203
dimasm93 <software.dimas_m@icloud.com>
2022-08-29 03:54:19 +0700

    pembahasan soal no 2

--- query/01-select/15-latihan-part2.sql
+++ query/01-select/15-latihan-part2.sql
@@ -11,3 +11,20 @@ from employees emp
          join departments dep on emp.department_id = dep.department_id
 where salary + (salary * coalesce(commission_pct, 0)) >= 12000
 order by gaji_terima;
+
+-- pembahasan soal no 2
+select emp.employee_id                                           "Kode Karyawan",
+       concat(emp.first_name, ' ', emp.last_name)                nama_karyawan,
+       dep.department_id                                         "Kode Department",
+       dep.department_name                                       "Nama Department",
+       emp.job_id                                                "Kode Jabatan",
+       job.job_title                                             "Nama Jabatan",
+       case
+           when emp.manager_id is null then 'Saya gak punya manager!'
+           else
+               concat(man.first_name, ' ', man.last_name) end as manager_name
+from employees emp
+         join departments dep on emp.department_id = dep.department_id
+         join jobs job on emp.job_id = job.job_id
+         left join employees man on emp.manager_id = man.employee_id
+order by manager_name, nama_karyawan;
