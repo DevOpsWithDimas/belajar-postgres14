61b2c0395b820adefb5afb985d7124851bda400f
dimasm93 <software.dimas_m@icloud.com>
2022-11-09 13:42:31 +0700

    insert statement with specify data types

--- query/02-data-manipulation/02-insert-statement.sql
+++ query/02-data-manipulation/02-insert-statement.sql
@@ -1,7 +1,13 @@
 INSERT INTO employees(email, first_name, last_name, salary, job_id)
-values ('DIMAS', initcap('dimas'), initcap('maryanto'), 15000, upper('it_prog'));
+values ('DIMAS1', initcap('dimas'), initcap('maryanto'), '15000', upper('it_prog'));
 
 select *
 from employees
-where email = 'DIMAS';
+where email = 'DIMAS1';
 
+INSERT INTO job_history(employee_id, start_date, job_id)
+VALUES (1, '2018-07-15 12:10:00', 'IT_PROG');
+
+SELECT *
+FROM job_history
+where employee_id = 1;
