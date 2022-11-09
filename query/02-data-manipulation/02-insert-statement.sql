70ba7d5a4961167de4b19bb5a43f84fe6894f76b
dimasm93 <software.dimas_m@icloud.com>
2022-11-09 14:54:43 +0700

    update documentation

--- query/02-data-manipulation/02-insert-statement.sql
+++ query/02-data-manipulation/02-insert-statement.sql
@@ -1,3 +1,4 @@
+-- insert with specify columns using custom orders
 INSERT INTO employees(email, first_name, last_name, salary, job_id)
 values ('DIMAS1', initcap('dimas'), initcap('maryanto'), '15000', upper('it_prog'));
 
@@ -5,8 +6,12 @@ select *
 from employees
 where email = 'DIMAS1';
 
+-- insert with specify data type with convert from date to timestamp
 INSERT INTO job_history(employee_id, start_date, job_id)
-VALUES (1, '2018-07-15 12:10:00', 'IT_PROG');
+VALUES (1, '2016-07-15', 'IT_PROG');
+
+INSERT INTO job_history(employee_id, start_date, job_id)
+VALUES (1, '2018-07-15 12:15:00', 'IT_PROG');
 
 SELECT *
 FROM job_history
