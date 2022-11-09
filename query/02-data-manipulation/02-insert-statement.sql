424f9d8eeae40166f7264fcec0ded4a157100ea1
dimasm93 <software.dimas_m@icloud.com>
2022-11-09 13:18:24 +0700

    rollback data

--- query/02-data-manipulation/02-insert-statement.sql
+++ query/02-data-manipulation/02-insert-statement.sql
@@ -1,13 +1,7 @@
 INSERT INTO employees(email, first_name, last_name, salary, job_id)
-values ('DIMAS1', initcap('dimas'), initcap('maryanto'), '15000', upper('it_prog'));
+values ('DIMAS1', initcap('dimas'), initcap('maryanto'), 15000, upper('it_prog'));
 
 select *
 from employees
 where email = 'DIMAS1';
 
-insert into job_history(employee_id, start_date, job_id)
-values (2, '2016-07-15', 'IT_PROG');
-
-select *
-from job_history
-where employee_id = 2;
