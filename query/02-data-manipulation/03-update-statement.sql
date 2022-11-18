9eafff589c9c47cacc6d0b6a8cd2549670466934
dimasm93 <software.dimas_m@icloud.com>
2022-11-18 08:20:16 +0700

    update statement with column-list syntax

--- query/02-data-manipulation/03-update-statement.sql
+++ query/02-data-manipulation/03-update-statement.sql
@@ -1,5 +1,5 @@
 --- update with default value
-select employee_id, first_name, last_name, salary, job_id
+select employee_id, first_name, last_name, salary, job_id, commission_pct
 from employees
 where department_id = 10;
 
@@ -11,7 +11,7 @@ alter table employees
     alter column salary SET default 4400;
 
 --- update with sub-select clause
-select min_salary, job_id
+select min_salary, max_salary, job_id
 from jobs
 where job_id in (select job_id
                  from employees
@@ -23,4 +23,21 @@ WHERE department_id = 10;
 
 UPDATE employees emp
 SET salary = salary + 1000
+WHERE department_id = 10;
+
+--- update with column-list syntax
+UPDATE employees emp
+SET (salary, commission_pct) = (
+    select max_salary, 0.2 as commission_pct
+    from jobs job
+    where emp.job_id = job.job_id)
+WHERE department_id = 10;
+
+-- eq
+UPDATE employees emp
+SET salary         = (
+    select max_salary
+    from jobs job
+    where emp.job_id = job.job_id),
+    commission_pct = 0.2
 WHERE department_id = 10;
