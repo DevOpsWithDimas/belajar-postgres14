cb9e2df7ad196262b5e1edf232562b541309375b
dimasm93 <software.dimas_m@icloud.com>
2022-11-18 07:50:40 +0700

    update with sub-SELECT clause

--- query/02-data-manipulation/03-update-statement.sql
+++ query/02-data-manipulation/03-update-statement.sql
@@ -1,5 +1,5 @@
 --- update with default value
-select employee_id, first_name, last_name, salary
+select employee_id, first_name, last_name, salary, job_id
 from employees
 where department_id = 10;
 
@@ -10,3 +10,17 @@ WHERE department_id = 10;
 alter table employees
     alter column salary SET default 4400;
 
+--- update with sub-select clause
+select min_salary, job_id
+from jobs
+where job_id in (select job_id
+                 from employees
+                 where department_id = 10);
+
+UPDATE employees emp
+SET salary = (select min_salary from jobs job where emp.job_id = job.job_id)
+WHERE department_id = 10;
+
+UPDATE employees emp
+SET salary = salary + 1000
+WHERE department_id = 10;
