20bb05a95215ab4523b8949d5a609c956e6e3b84
dimasm93 <software.dimas_m@icloud.com>
2023-02-09 01:41:08 +0700

    pembahasan soal no 2

--- query/02-data-manipulation/07-quis-3.sql
+++ query/02-data-manipulation/07-quis-3.sql
@@ -5,3 +5,15 @@ values ('ID', 'Indonesia', 3);
 select *
 from countries
 where country_id = 'ID';
+
+--- soal no 2
+INSERT INTO locations(location_id, street_address, postal_code, city, state_province, country_id)
+values (6230, 'Cinunuk', 40626, 'Kab. Bandung', 'Jawa Barat', 'ID'),
+       (6231, 'Ujung Berung', 40521, 'Kab. Bandung', 'Jawa Barat', 'ID'),
+       (6233, 'Margahayu Raya', 40525, 'Kota. Bandung', 'Jawa Barat', 'ID'),
+       (6234, 'Blok M', 40220, 'Jakarta Selatan', 'DKI Jakarta', 'ID'),
+       (6235, 'Slipi', 40221, 'Jakarta Selatan', 'DKI Jakarta', 'ID');
+
+select *
+from locations
+where location_id in (6230, 6231, 6233, 6234, 6235);
