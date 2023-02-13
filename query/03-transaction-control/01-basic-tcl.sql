dedb3a6ba0266c58b61763467753bbe5a04c0e9a
dimasm93 <software.dimas_m@icloud.com>
2023-02-14 03:18:16 +0700

    using commit statement

--- query/03-transaction-control/01-basic-tcl.sql
+++ query/03-transaction-control/01-basic-tcl.sql
@@ -2,13 +2,17 @@
 INSERT INTO regions(region_id, region_name)
 VALUES (10, 'Other');
 
-select * from regions where region_id = 10;
+select *
+from regions
+where region_id = 10;
 
 --- manual behavior by editor
 INSERT INTO regions(region_id, region_name)
 VALUES (11, 'Other 2');
 
-select * from regions where region_id = 11;
+select *
+from regions
+where region_id = 11;
 
 --- manual behavior by query
 BEGIN;
@@ -16,4 +20,27 @@ BEGIN;
 INSERT INTO regions(region_id, region_name)
 VALUES (12, 'Other 3');
 
-select * from regions where region_id = 12;
+select *
+from regions
+where region_id = 12;
+
+
+--- transaction with commit clause
+BEGIN;
+
+INSERT INTO regions(region_id, region_name)
+VALUES (12, 'Other 3');
+
+select *
+from regions
+where region_id = 12;
+
+UPDATE regions
+SET region_name = 'Asia Pasific'
+where region_id = 10;
+
+select *
+from regions
+where region_id = 10;
+
+COMMIT;
