629aae8e82b574ca01ded003da2a73fd90d4a4ce
dimasm93 <software.dimas_m@icloud.com>
2023-02-18 02:15:26 +0700

    using savepoint clause

--- query/03-transaction-control/01-basic-tcl.sql
+++ query/03-transaction-control/01-basic-tcl.sql
@@ -64,3 +64,41 @@ INSERT INTO regions(region_id, region_name)
 VALUES (10, 'Other');
 
 COMMIT;
+
+
+--- using savepoint
+BEGIN;
+
+INSERT INTO regions(region_id, region_name)
+VALUES (14, 'Other 4');
+
+UPDATE regions
+SET region_name = 'Asia Pasific UPDATED'
+where region_id = 10;
+
+select *
+from regions
+where region_id in (10, 14);
+
+savepoint trx1;
+
+UPDATE regions
+SET region_name = 'Asia Pasific UPDATED ke 2'
+where region_id = 10;
+
+select *
+from regions
+where region_id in (10, 14);
+
+savepoint trx2;
+
+INSERT INTO regions(region_id, region_name)
+VALUES (10, 'Other');
+
+rollback to savepoint trx1;
+
+UPDATE regions
+SET region_name = 'Asia Pasific'
+where region_id = 10;
+
+commit;
