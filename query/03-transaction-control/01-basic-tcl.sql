9093756ec959bdd70629a7b74d6f7ec198f97f30
dimasm93 <software.dimas_m@icloud.com>
2023-02-18 01:42:07 +0700

    using rollback clause

--- query/03-transaction-control/01-basic-tcl.sql
+++ query/03-transaction-control/01-basic-tcl.sql
@@ -44,3 +44,23 @@ from regions
 where region_id = 10;
 
 COMMIT;
+
+--- rollback with schenario human error
+BEGIN;
+UPDATE regions
+SET region_name = 'Asia Pasific UPDATED'
+where region_id = 10;
+
+select *
+from regions
+where region_id = 10;
+
+ROLLBACK;
+
+--- rollback with schenario runtime error
+BEGIN;
+
+INSERT INTO regions(region_id, region_name)
+VALUES (10, 'Other');
+
+COMMIT;
